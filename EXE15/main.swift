c8455da9c433a93deb1c48c8a31bac7230e7fe3a
Shinichirou Yamazaki <shinichirou.yamazaki@gmail.com>
2017-10-14 16:39:43 +0900

    Initial Commit

---
+++ EXE15/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EXE15
+//
+//  Created by shinichirou.yamazaki. on 2017/10/14.
+//  Copyright © 2017 shinichirou.yamazaki. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
