baf2381f1ebe685c31e5d07151aeb3a683a6fd01
山崎真一郎 <yamazaki@yamazakishinichirounoimac.local>
2017-10-14 17:46:11 +0900

    演習問題1.5の解

--- EXE15/main.swift
+++ EXE15/main.swift
@@ -8,10 +8,26 @@
 
 import Foundation
 
-print("Hello, World!")
 
-print("test")
+let Y = 4.8
+let Xn = [1.0, 2.0, 3.0, 5.5, 10.2]
 
-print("test2")
+
+
+var t = Xn[0]
+var s = abs(Y - Xn[0])
+
+for x in Xn
+{
+    if (abs(x - Y) < s)
+    {
+        t = x
+        s = abs(x - Y)
+    }
+}
+
+print("\(t):\(s)")
+
+
 
 
